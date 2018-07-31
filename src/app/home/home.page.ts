a1a3fe4a0abf235dc3b73ab57a22a5fd79290d61
Will Strinz <wstrinz@gmail.com>
2018-07-31 14:42:58 -0500

    wip auth plumbing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
 })
-export class HomePage {}
+export class HomePage {
+  public username: string = '';
+  public password: string = '';
+
+  constructor(public authService: AuthService) {}
+
+  public submit() {
+    this.authService.cookieLogin(this.username, this.password);
+  }
+}
